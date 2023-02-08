eb9fdd85d9235fedfbd5a0c748e6a50db1cf3d28
Leo Davidson <94934268+ldavidson2@users.noreply.github.com>
2023-02-08 14:31:56 -0600

    changing some file names

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-import {Routes, Route} from 'react-router-dom';
-import Login from './Login';
-import Profile from './Profile';
-import { RequireToken} from './Auth'
+import logo from "./logo.svg";
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Login from "./Login";
+import Profile from "./Profile";
+import { RequireToken } from "./Auth";
 function App() {
   return (
-    <div className ="App">
-    <Routes>
-      <Route path="/" 
-      element = {<Login/>}/>
-      <Route path="/profile"
-       element = {
-       <RequireToken>
-         <Profile/>
-       </RequireToken>
-      }
-      />
-    </Routes>
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route
+          path="/profile"
+          element={
+            <RequireToken>
+              <Profile />
+            </RequireToken>
+          }
+        />
+      </Routes>
     </div>
   );
 }
