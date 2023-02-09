a50e885272c2cc1466339a863baee953195be57d
Leo Davidson <94934268+ldavidson2@users.noreply.github.com>
2023-02-09 09:14:20 -0600

    Added new database

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,25 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "fastapireacttest": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "storage": {
+    "fastapireactDB": {
+      "Arn": "string",
+      "Name": "string",
+      "PartitionKeyName": "string",
+      "PartitionKeyType": "string",
+      "Region": "string",
+      "SortKeyName": "string",
+      "SortKeyType": "string",
+      "StreamArn": "string"
+    }
+  }
+}
--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Login from "./Login";
 import Profile from "./Profile";
 import { RequireToken } from "./Auth";
+import { Amplify, DataStore, Predicates } from "aws-amplify";
+
+async function onQuery() {
+  const posts = await DataStore.query(Test);
+
+  console.log(posts);
+}
+
 function App() {
+
+
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route
-          path="/profile"
-          element={
-            <RequireToken>
-              <Profile />
-            </RequireToken>
-          }
-        />
-      </Routes>
+      <header className="App-header">
+        <div>
+          <input type="button" value="NEW" />
+          <input type="button" value="DELETE ALL" />
+          <input type="button" value="QUERY rating > 4" onClick={onQuery} />
+        </div>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
