0aaf9796b49f3665cade81e90ead9b380fd12530
Leo Davidson <94934268+ldavidson2@users.noreply.github.com>
2023-02-10 13:22:12 -0600

    Revert "Connected to database"
    This reverts commit 82954835b30d738e2f08c634e9dfd705c04a4d72.

--- src/App.js
+++ src/App.js
@@ -3,12 +3,37 @@ import { Routes, Route } from "react-router-dom";
 import Login from "./ui-components/Login";
 import Profile from "./Profile";
 import { RequireToken } from "./Auth";
-import Message from "./components/message";
+import { Amplify, DataStore, Predicates } from "aws-amplify";
+
+async function onQuery() {
+  const posts = await DataStore.query(Test);
+
+  console.log(posts);
+}
 
 function App() {
+
+
   return (
     <div className="App">
-      <Message />
+      <header className="App-header">
+        <div>
+          <input type="button" value="NEW" />
+          <input type="button" value="DELETE ALL" />
+          <input type="button" value="QUERY rating > 4" onClick={onQuery} />
+        </div>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
