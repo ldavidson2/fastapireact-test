8ea41764e52cc5b46f06009c7543fdbc06bc3f6b
Leo Davidson <94934268+ldavidson2@users.noreply.github.com>
2023-02-10 13:18:47 -0600

    Revert "test"
    This reverts commit f3273e11d548d45b17c113e17a1368c4fa0520ea.

--- backend/venv/main.py
+++ backend/venv/main.py
@@ -30,7 +30,7 @@ userData = str(data['Item'])
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins="https://main.d1wvqt2twpsc7d.amplifyapp.com/",
+    allow_origins="http://localhost:3000/",
     allow_credentials=True,
     allow_methods=["GET"],
     allow_headers=["Content-Type","application/xml"],
